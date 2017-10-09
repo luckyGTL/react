a78237aaecf777c3db09b61375f2b2c7cc98ed7f
luckyGTL <653945712@qq.com>
2017-10-09 20:13:32 +0800

    12

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js'
+  },
+  module: {
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
+}
+};
